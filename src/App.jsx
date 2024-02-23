b6d3fd35f00aed64be44f9b92ffd83fa2a980057
Noah <noahglennmaldonado@gmail.com>
2024-02-23 09:16:34 -0500

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
 
 function App() {
-  return <div className="text-4xl text-red-500">HI</div>;
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
