ae6a1c1dc6b76a03fb18516ba6cd96293e864581
Noah <noahglennmaldonado@gmail.com>
2024-02-23 08:32:37 -0500

    added software and setup folder layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return <div className="text-4xl text-red-500">HI</div>;
 }
 
 export default App;
