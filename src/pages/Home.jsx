2f174331e53b36e928c95bbcd51be6a8eae3c492
Noah <noahglennmaldonado@gmail.com>
2024-02-29 04:58:59 -0500

    added fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export function Home() {
-  return <h1></h1>;
+  return (
+    <div className="home h-[90vh] bg-img1 flex items-center p-6 bg-cover bg-no-repeat">
+      <div className="headerContainer text-white ">
+        <h1 className="title text-8xl mb-8 font-playfair ">E-Shop</h1>
+        <p className="title-text text-3xl ">Buy what you need!</p>
+        <Link to={"/store"}>
+          <button className="shop-btn  h-14 w-44 mt-10 bg-[#90e0ef] hover:text-[#fb8500] rounded-md text-white font-bold">
+            SHOP NOW
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        img1: "url('./assets/retail_shopping_3.29.jpg')",
+      },
+      fontFamily: {
+        playfair: ["Playfair Display"],
+        lobster: ["Lobster", "sans-serif"],
+        kalam: ["Kalam", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
